e3f5fee8fe37cbbfef10512b0fc3a5f0efb5034e
parkervg <44219290+parkervg@users.noreply.github.com>
2019-02-07 16:19:50 -0800

    Update Articles_Dataframe_Formatting.py

--- Articles_Dataframe_Formatting.py
+++ Articles_Dataframe_Formatting.py
@@ -31,11 +31,6 @@ df_from_each_file = (pd.read_csv(f) for f in all_files)
 concatenated_df = pd.concat(df_from_each_file, ignore_index = True)
 
 
-# In[26]:
-
-
-concatenated_df.to_csv("ALLARTICLES.csv")
-
 
 # In[32]:
 
